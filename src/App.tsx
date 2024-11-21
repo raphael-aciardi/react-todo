fa608ab8ebb73d5080811f79044a8bd1a9f89620
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2024-11-20 21:56:36 -0300

    Creating the first component

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './global.css'
+import {Header} from "./componetnts/Header.tsx";
+function App() {
+
+  return (
+    <Header />
+  )
+}
+
+export default App
---
+++ src/componetnts/Header.tsx
@@ -0,0 +1,8 @@
+
+export function Header() {
+  return (
+    <header className="header">
+      <h1>My Todo List</h1>
+    </header>
+  );
+}
