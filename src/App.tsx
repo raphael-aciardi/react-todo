9f458b478fa02b4d0f2ff992de1365eeeedb9a58
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2024-11-23 01:26:24 -0300

    Creating inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import {Header} from "./componetnts/Header.tsx";
+import { Button } from "./componetnts/Button.tsx";
 import './App.module.css'
 import './global.css'
 import { Input } from "./componetnts/Input.tsx";
 import styles from './App.module.css'
 function App() {
-
   return (
       <div className={styles.appContainer}>
-        <Header />
-        <div className={styles.inputContainer}>
-          <Input />
+        <div className={styles.headerConteiner}>
+          <Header />
+          <div className={styles.inputContainer}>
+            <Input />
+            <Button />
+          </div>
         </div>
       </div>
   )
---
+++ src/componetnts/Button.tsx
@@ -0,0 +1,10 @@
+import styles from './Button.module.css'
+import { PlusCircle  } from "phosphor-react";
+
+
+export function Button() {
+  return (
+    <button className={styles.button}><span>Criar</span><PlusCircle size={16} /></button>
+ 
+  )
+}
